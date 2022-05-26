9ecdf75bb8e7c808eab773c7a18f6b6fea74839d
Laura Aguilar <laura.aguilar90@gmail.com>
2022-05-25 22:20:14 -0300

    Desafio 5

---
+++ Desafio5/otro/routes/productoRouter.js
@@ -0,0 +1,62 @@
+import express from 'express';
+import Producto from '../controllers/products.js';
+
+function crearRouterProductos() { 
+        const router = express.Router();
+        const producto = new Producto();
+
+        router.get("/vista", (req, res) => {
+                const productosActivos = producto.leer();  
+                console.log(productosActivos)   
+                res.render('main', {
+                        vista: "leer",
+                        productos: productosActivos,
+                });
+        });
+
+        router.get("/vista/cargar", (req, res) => {    
+                res.render('main', {
+                        vista: "cargar"
+                });
+        });
+
+        router.get("/", (req, res) => {
+                const productosActivos = producto.leer();
+                if (!productosActivos) {return res.status(404).json({error: 'no hay prodructos cargados'})};
+                res.json(productosActivos);
+        });
+
+        router.get("/:id", (req, res) => {
+                const id = req.params.id;
+                const productoBuscado = producto.leerID(id);
+                if (productoBuscado) {return res.json(productoBuscado)};
+                res.status(404).json({error: 'producto no encontrado'});
+        })
+
+        router.post("/",(req, res) => {
+                const data = req.body;
+                console.log(data);
+                if (producto.agregar(data)) {
+                        if (data.form === "1") return res.redirect("/api/productos/vista/cargar")
+                        return res.status(201).json(data)
+                };
+                res.status(400).send();
+        });
+
+        router.delete("/:id", (req, res) => {
+                const id = req.params.id;
+                if (producto.eliminar(id)) {return res.status(200).json({exito: 'producto eliminado'})};
+                res.status(400).json({error: 'producto no existe'});
+        })
+
+        router.put("/:id", (req, res) => {
+                const id =req.params.id;
+                const data = req.body;
+                if (producto.actualizar(id, data)) {return res.status(201).json(data)};
+                res.status(400).json({error: 'producto NO actualizado'});
+        })
+
+        return router;
+}
+
+export {crearRouterProductos}
---
+++ Desafio5/pugjs/api.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import {crearRouterProductos} from './routes/productoRouter.js';
+
+const PORT = 8080;
+const app = express();
+
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use("/api/productos", crearRouterProductos());
+
+const server = app.listen(PORT, () => {
+    console.log(`Servidor http escuchando en el puerto ${server.address().port}`);
+})
+
+server.on("error", error => console.log(`Error en servidor ${error}`));
